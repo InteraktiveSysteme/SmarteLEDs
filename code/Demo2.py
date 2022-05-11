2ce6a2e3fb78ad5867abf713d6b619b45d913fa3
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-05-11 18:22:23 +0200

    Demo Blender python scripts

---
+++ code/Demo2.py
@@ -0,0 +1,6 @@
+import bpy #imports Blender library
+
+
+#bpy.context.scene.render.resolution_x = 1000 #perhaps set resolution in code
+# bpy.context.scene.render.resolution_y = 1000
+# bpy.context.scene.cycles.samples = 20

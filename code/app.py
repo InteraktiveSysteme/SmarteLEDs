43ab5b85cedb4f8bab1b2b1c5e67ee52ef6fdbb8
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-05-13 18:41:00 +0200

    read lamp directory and list lamp titles based on markdown files

--- code/app.py
+++ code/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
 import time
+import os
+import re
 
-app = Flask(__name__)
+app = Flask("SamuelsName")
 
 
 @app.route('/')
@@ -14,6 +16,46 @@ def render():
 	exec(open("Demo.py").read())
 	return render_template('render.html')
 
+@app.route('/lamp')
+def lamp():
+        return render_template('lamp.html')
+
+
+
+print("")
+print("")
+print("")
+entries = os.listdir('static/lamps')
+pattern = ".*\.md"
+filtered = []
+for e in entries:
+    if re.match(".*\.md", e):
+        filtered.append(re.sub("\..*", "", e))
+del entries
+
+print(filtered)
+print("")
+print("")
+print("")
+print("")
+print("")
+
+
+
+
+
+#print(re.sub("lamp01.md", lambda entry: entry, entries))
+
+#a = 'the quick brown fox jumped over the green lazy python'
+#print(re.sub("(\w+)",
+#             lambda x: x.group(1).capitalize(),
+#             a))
+#print(list(filteredlist))
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
---
+++ code/static/lamps/lamp01.md
@@ -0,0 +1,9 @@
+# Super Lampe
+
+{Eig1} Versandkosten: 4,90 € 
+{Eig2} versandfertig innerhalb von 2-4 Werktagen 
+- Kein Leuchtmittel im Lieferumfang enthalten. 
+- Artikelnummer 273397 
+- Dimmbar externen Dimmer (Nicht Inklusive) 
+- Abmessungen Breite: 12 cm Länge: 23 cm Höhe: 12 cm 
+- Fassung E27 

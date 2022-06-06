6908870af0a4f9da40226c44c11a538b4d11f9bd
DerLukasDecker <ldeckerluss@gmail.com>
2022-06-06 14:18:02 +0200

    Experimenting with lights in room
    added MeshPhongMaterial, shadows and spotlights

--- code/static/js/room.js
+++ code/static/js/room.js
@@ -16,91 +16,82 @@ const frontBackGeo = new THREE.PlaneGeometry( width, height );
 const SideGeo = new THREE.PlaneGeometry( depth, height )
 const topBottomGeo = new THREE.PlaneGeometry( width, depth )
 const cubeGeo = new THREE.BoxGeometry( .25, 0.25, .25)
+const lightGeo = new THREE.BoxGeometry( .1, .1, .1 )
 
 // Materials
-const material = new THREE.MeshStandardMaterial( {color: 0xffff00, side: THREE.DoubleSide} )
+const material = new THREE.MeshPhongMaterial( {color: 0xffff00, side: THREE.DoubleSide} )
 material.metalness = 0.0
 material.roughness = 1.0
 material.color = new THREE.Color(0xFFFFFF)
 
-const material2 = new THREE.MeshStandardMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+const material2 = new THREE.MeshPhongMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
 material2.metalness = 0.7
 material2.roughness = 0.3
 material2.color = new THREE.Color(0x808080)
 
-const material3 = new THREE.MeshStandardMaterial(  )
+const material3 = new THREE.MeshPhongMaterial(  )
 material.metalness = 0.0
 material.roughness = 1.0
 material.color = new THREE.Color(0xFFFFFF)
 
-// Mesh
-//  Front and back wall
-/*const frontPlane = new THREE.Mesh(frontBackGeo, material2)
-frontPlane.position.z = depth / 2
-scene.add(frontPlane)
-
-const backPlane = new THREE.Mesh(frontBackGeo, material2)
-backPlane.position.z = - ( depth / 2 )
-scene.add(backPlane)
-
-//  Left side and right side
-const leftPlane = new THREE.Mesh(SideGeo, material2)
-leftPlane.rotation.y = Math.PI / 2
-leftPlane.position.x = - ( width / 2 )
-scene.add(leftPlane)
-
-const rightPlane = new THREE.Mesh(SideGeo, material2)
-rightPlane.rotation.y = - ( Math.PI / 2 )
-rightPlane.position.x = width / 2
-scene.add(rightPlane)
-
-//  Top and bottom
-const topPlane = new THREE.Mesh(topBottomGeo, material2)
-topPlane.rotation.x = Math.PI / 2
-topPlane.position.y = height / 2
-scene.add(topPlane)
-
-const bottomPlane = new THREE.Mesh(topBottomGeo, material2)
-bottomPlane.rotation.x = -( Math.PI / 2 )
-bottomPlane.position.y = - ( height / 2 )
-scene.add(bottomPlane)
-*/
 // Mesh and walls 
 //  Front and back wall
 const frontPlane = new THREE.Mesh(frontBackGeo, material3)
+frontPlane.castShadow = true
+frontPlane.receiveShadow = true
 frontPlane.rotation.x = Math.PI
 frontPlane.position.z = depth / 2
 scene.add(frontPlane)
 
 const backPlane = new THREE.Mesh(frontBackGeo, material3)
+backPlane.castShadow =true
+backPlane.receiveShadow = true
 backPlane.position.z = - ( depth / 2 )
 scene.add(backPlane)
 
 //  Left side and right side
 const leftPlane = new THREE.Mesh(SideGeo, material3)
+leftPlane.castShadow = true
+leftPlane.receiveShadow = true
 leftPlane.rotation.y = Math.PI / 2
 leftPlane.position.x = - ( width / 2 )
 scene.add(leftPlane)
 
 const rightPlane = new THREE.Mesh(SideGeo, material3)
+rightPlane.castShadow = true
+rightPlane.receiveShadow = true
 rightPlane.rotation.y = - ( Math.PI / 2 )
 rightPlane.position.x = width / 2
 scene.add(rightPlane)
 
 //  Top and bottom
 const topPlane = new THREE.Mesh(topBottomGeo, material3)
+topPlane.castShadow = true
+topPlane.receiveShadow = true
 topPlane.rotation.x = Math.PI / 2
 topPlane.position.y = height / 2
 scene.add(topPlane)
 
 const bottomPlane = new THREE.Mesh(topBottomGeo, material3)
+bottomPlane.castShadow = true
+bottomPlane.receiveShadow = true
 bottomPlane.rotation.x = -( Math.PI / 2 )
 bottomPlane.position.y = - ( height / 2 )
 scene.add(bottomPlane)
 
 // interior Mesh
 const cube = new THREE.Mesh( cubeGeo, material2 )
+cube.castShadow = true
+cube.receiveShadow = true
 scene.add( cube )
+
+const lightCube = new THREE.Mesh ( lightGeo, material3 )
+lightCube.position.set( 0.45, 0.2, 0 )
+scene.add( lightCube ) 
+
+const lightCube2 = new THREE.Mesh ( lightGeo, material3 )
+lightCube2.position.set( 0, 0.2, 0.45 )
+scene.add( lightCube2 ) 
 
 // creating the bounding box for the cube
 const box = new THREE.Box3( new THREE.Vector3(), new THREE.Vector3() )
@@ -126,31 +117,57 @@ loader.load("{{ url_for('static', filename='/static/Gltf/BarramundiFish.gltf' ) 
     scene.add(cubeMesh)
 })
 */
+//Lights
+
+// Spotlight 1
+const spotLight = new THREE.SpotLight( 0xff0000, 0.7, 8, -(Math.PI / 4), 0.3, 2 )
+spotLight.position.set( 0.45, 0.2, 0 )
+spotLight.castShadow = true
+const sLHelper = new THREE.SpotLightHelper( spotLight )
+scene.add( spotLight )
+scene.add( sLHelper )
+
+// Spotlight 2
+const spotLight2 = new THREE.SpotLight( 0x0000ff, 0.7, 8, -(Math.PI / 4), 0.3, 2 )
+spotLight2.position.set( 0, 0.2, 0.45 )
+spotLight2.rotation.y = - ( Math.Pi / 2 )
+spotLight2.castShadow = true
+const sLHelper2 = new THREE.SpotLightHelper( spotLight2 )
+scene.add( spotLight2 )
+scene.add( sLHelper2 )
+
+// Pointlight 
+// const pointLight = new THREE.PointLight( 0xffff00, 1 )
+// pointLight.position.set( 0, 0.2, 0 )
+// pointLight.castShadow = true
+// const pLHelper = new THREE.PointLightHelper( pointLight )
+// scene.add( pointLight )
+// scene.add( pLHelper )
 
 // Light 1
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
-pointLight.intensity = 1
+// const pointLight = new THREE.PointLight(0xffffff, 0.1)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
+// scene.add(pointLight)
+// pointLight.intensity = 1
 
 //Light 2
 
-const pointLight2 = new THREE.PointLight(0xff0000, 10)
-pointLight2.position.set(-1.86,1,-1.65)
-scene.add(pointLight2)
+// const pointLight2 = new THREE.PointLight(0xff0000, 10)
+// pointLight2.position.set(-1.86,1,-1.65)
+// scene.add(pointLight2)
 
 // const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1) //is a reference, where the point light is?
 // scene.add(pointLightHelper)
 
 //Light 3
 
-const pointLight3 = new THREE.PointLight(0x0000ff, 10)
+// const pointLight3 = new THREE.PointLight(0x0000ff, 10)
 
-pointLight3.position.set(1.92,-1.31,0.34)
-scene.add(pointLight3)
+// pointLight3.position.set(1.92,-1.31,0.34)
+// scene.add(pointLight3)
 
 // const pointLightHelper3 = new THREE.PointLightHelper(pointLight3, 1) //is a reference, where the point light is?
 // scene.add(pointLightHelper3)
@@ -220,6 +237,8 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.VSMShadowMap
 
 /**
  * Animate

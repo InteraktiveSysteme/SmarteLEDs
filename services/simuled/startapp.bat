78a10190fd1b8fee3870aec56e4f88e6464b0c19
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-08-15 16:42:05 +0200

    Clean main branch for docker project --> move documentation word doc to unidox

--- services/simuled/startapp.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set PYTHONDONTWRITEBYTECODE=1
-$env:PYTHONDONTWRITEBYTECODE = "1"
-set FLASK_APP=project\__init__.py
-$env:FLASK_APP = "project\__init__.py"
-pip install -r requirements.txt
-flask run
-
-PAUSE
--- services/simuled/startapp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export PYTHONDONTWRITEBYTECODE=1
-export PYTHONUNBUFFERED=1
-export FLASK_APP=project/__init__.py
-pip install -r requirements.txt
-flask run

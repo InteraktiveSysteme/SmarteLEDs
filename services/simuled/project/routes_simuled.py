25337e1f8c628cdc4c827e125d2aed101f9545b7
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-08-15 22:16:43 +0200

    Updated routes_simuled.py (not updated during merge for some reason)

--- services/simuled/project/routes_simuled.py
+++ services/simuled/project/routes_simuled.py
@@ -8,14 +8,17 @@ import uuid as uuid
 import os
 import json
 from flask_login import *
+from project import forms
 
-
-def child():  # put application's code here
-    return render_template('child.html')
+##
+# @brief This function creates an Cart element for a specific lamp you want to shop
+# @param id is the lampID
+# ...
+# @return the HTML template
 
 def shopLamp(id):  # put application's code here
-
     if current_user.is_authenticated:
+        # create a cart obj in the Database
         cart = Cart(userID=current_user.userID, lampID=id, amount=1)
         db.session.add(cart)
         db.session.commit()
@@ -26,53 +29,97 @@ def shopLamp(id):  # put application's code here
         if request.cookies.get('cart') is None:
             cartTEMP = []
             cartTEMP.append(int(id))
-            resp = make_response(render_template("shop.html", lamps=lamps), 200)
+            resp = make_response(render_template(
+                "shop.html", lamps=lamps), 200)
         else:
             cartTEMP = json.loads(request.cookies.get('cart'))
             cartTEMP.append(int(id))
-            resp = make_response(render_template("shop.html", lamps=lamps), 200)
+            resp = make_response(render_template(
+                "shop.html", lamps=lamps), 200)
         cartJSON = json.dumps(cartTEMP)
         resp.set_cookie('cart', cartJSON)
 
     return resp
 
+##
+# @brief This function gives the form information to the preSim template
+# @return the HTML template
+def preSim():
+    if request.method == "GET":
+        # GET branch to return the Template and Form
+        form = forms.RoomForm()
+        return render_template('preSim.html', form=form)
+
+    if request.method == "POST":
+        # POST branch to get the Room Informations
+        width = request.form["width"]
+        height = request.form["height"]
+        depth = request.form["depth"]
+
+        return render_template('Simuled.html', width=width, height=height, depth=depth)
+
+##
+# @brief This function handles the Shopping Cart logic
+# @return the HTML template
 def shoppingCart():
-    list = []
+    form = forms.OrderForm()
     if current_user.is_authenticated:
-        print("getShoppingCart")
+        if request.method == "POST":
+            #The following Shipping Informations are useful if you want to deploy this Software
+            firstName = request.form["firstName"]
+            lastName = request.form["lastName"]
+            country = request.form["country"]
+            adress = request.form["adress"]
+            postalCode = request.form["postalCode"]
+            carts = Cart.query.filter_by(userID=current_user.userID)
+            #deleting all objects from the cart
+            for cart in carts:
+                db.session.delete(cart)
+            db.session.commit()
+            flash(message="Thanks for ordering!")
+            return render_template('index.html')
 
-        lamps = Cart.query.filter_by(userID = current_user.userID)
+        if request.method == "GET":
+            carts = Cart.query.filter_by(userID=current_user.userID)
+            lamps = []
+            for cart in carts:
+                lamps.append(Lamp.query.get_or_404(cart.lampID))
+            summe = 0
+            for lamp in lamps: #calculation of the final sum
+                if type(lamp.lampPrice) == float or type(lamp.lampPrice) == int:
+                    summe += lamp.lampPrice
+                else:
+                    summe += float(lamp.lampPrice.replace(",", "."))
+            return render_template('shoppingCartV3.html', lamps=lamps, summe=round(summe, 2), form=form)
 
-        for lamp in lamps:
-
-            if lamp in list:
-                print("doppel")
-            else:
-                print("einzel")
-                list.append(Lamp.query.get_or_404(lamp.lampID))
     else:
         if (request.cookies.get('cart') is None):
             return render_template('shoppingCart.html')
         else:
             cartTEMP = json.loads(request.cookies.get('cart'))
             for i in cartTEMP:
-                list.append(Lamp.query.get(i))
+                lamps.append(Lamp.query.get(i))
 
-    return render_template('shoppingCart.html', lamps=list)
+    summe = 0
+    for lamp in lamps:
+        print(type(lamp.lampPrice))
+        if type(lamp.lampPrice) == float or type(lamp.lampPrice) == int:
+            summe += lamp.lampPrice
+        else:
+            summe += float(lamp.lampPrice.replace(",", "."))
+    return render_template('shoppingCartV3.html', lamps=lamps, summe=round(summe, 2), form=form)
 
 
 def test():  # put application's code here
     return render_template('test.html')
 
+
 def expose_gltf(file):
     return send_from_directory(os.path.join(app.root_path, 'static/Gltf'), file)
 
+
 def testglb():
     return render_template('testglb.html')
-
-def bpyrender():
-    exec(open("Demo.py").read())
-    return render_template('render.html')
 
 
 def index():  # put application's code here
@@ -86,26 +133,29 @@ def render():
 
 
 def simuled():
-    return render_template('test.html')
+    lamps = Lamp.query.order_by(Lamp.timeStamp)
+    return render_template('simuled.html', lamps=lamps)
 
 
+##
+# @brief This function handles the addLamp logic. It provides the feature of adding a new Lamp
+# @return the HTML template
 @login_required
 def addLamp():
     if request.method == "GET":
-        return render_template('addLamp.html')
+        form = forms.AddLampForm()
+        return render_template('addLamp.html', form=form)
     if request.method == "POST":
         name = request.form["name"]
         img = request.files["img"]
         gltf = request.files["gltf"]
-
-        text = request.form["shorttext"]
-        longtext = request.form["longtext"]
+        text = request.form["shortText"]
+        longtext = request.form["longText"]
         price = request.form["price"]
         gltfName = secure_filename(img.filename)
         savegltfName = str(uuid.uuid1()) + "_" + gltfName
         gltfName = savegltfName
         gltf.save(os.path.join(app.config['UPLOAD_FOLDER'], savegltfName))
-
         imgName = secure_filename(img.filename)
         saveName = str(uuid.uuid1()) + "_" + imgName
         imgName = saveName
@@ -118,8 +168,8 @@ def addLamp():
         lamp = Lamp.query.order_by(Lamp.timeStamp)
         lamps = Lamp.query.order_by(Lamp.timeStamp)
         flash(message="Lamp added!")
-
-        return render_template("addLamp.html"), 200
+        form = forms.AddLampForm()
+        return render_template("addLamp.html", form=form), 200
 
 
 def shop():
@@ -127,7 +177,9 @@ def shop():
 
     return render_template("shop.html", lamps=lamps), 200
 
-
+##
+# @brief This function handles the adminLogic
+# @return the HTML template
 @login_required
 def admin():
     if current_user and current_user.admin:
@@ -139,11 +191,14 @@ def admin():
 
 
 def registerPage():
-    return render_template('registerPage.html')
+    form = forms.RegisterForm()
+    return render_template('registerPage.html', form=form)
 
 
-# liste mit allen lamp und
-# uri parameter /lamp/{id}
+##
+# @brief This function handles the adminLogic
+# @param id is the LampID that you want to show in detail
+# @return the HTML template
 def lamp(id):
     # funktion erstellt dirliste mit allen lampennamen
     lamp = Lamp.query.get_or_404(id)
@@ -171,7 +226,9 @@ def lamp(id):
                            name=lamp.lampName, text=lamp.lampText, price=lamp.lampPrice, img=lamp.imgName,
                            longText=lamp.lampLongText)
 
-
+##
+# @brief This function handles the register Logic
+# @return the HTML template
 def register():
     username = request.form['name']
     password = request.form["password"]
@@ -187,7 +244,8 @@ def register():
         admin = True
     login = request.form.get("loginbutton")
     register = request.form.get("registerbutton")
-    print("username: ", username, "  password: ", password, "login: ", login, "  register: ", register)
+    print("username: ", username, "  password: ", password,
+          "login: ", login, "  register: ", register)
     user = User(userName=username, password=password, admin=admin)
     print(login)
 

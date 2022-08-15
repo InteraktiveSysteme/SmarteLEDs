d7d1a9bacf5f56d0b0464cab2ae9948b1664e9ce
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-08-15 19:02:58 +0200

    Fix sqlite path to point by relative path to SQLTerror.db inside project folder

--- services/simuled/project/config.py
+++ services/simuled/project/config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://SQLTerror.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///SQLTerror.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_ECHO = True
     SECRET_KEY = os.urandom(12)

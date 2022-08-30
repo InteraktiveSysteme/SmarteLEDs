83140bae807aed9d5b1829e0136d14dbe9bbb40d
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-08-31 01:19:46 +0200

    add webpack bundler

---
+++ webpack/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+	entry: { 'bundle.js': [
+			path.resolve(__dirname, '../three.module.js'),
+			path.resolve(__dirname, '../GLTFLoader.js'),
+			path.resolve(__dirname, '../DragControls.js'),
+			path.resolve(__dirname, '../RotationControls.js'),
+			path.resolve(__dirname, '../Controls.js'),
+			path.resolve(__dirname, '../FirstPerson.js'),
+			path.resolve(__dirname, '../Turntable.js'),
+			path.resolve(__dirname, '../Create.js'),
+			path.resolve(__dirname, '../GUI_Objects.js'),
+			path.resolve(__dirname, '../State.js'),
+			path.resolve(__dirname, '../Run.js')
+	            ]
+	},
+	output: {
+		filename: '3droom.js',
+		path: path.resolve(__dirname, '../'),
+	},
+};

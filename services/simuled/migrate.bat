3b38371028a0b16af9384100f00335092af39ab5
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-08-31 13:16:21 +0200

    remove unused files from merge

--- services/simuled/migrate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set PYTHONDONTWRITEBYTECODE=1
-$env:PYTHONDONTWRITEBYTECODE = "1"
-set FLASK_APP=project\__init__.py
-$env:FLASK_APP = "project\__init__.py"
-flask db migrate -m "Migrate "
-flask db upgrade
-
-PAUSE
